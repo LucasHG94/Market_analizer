9d7eaa3f32a3a4f3ab24a790cbacc3593584325a
sturm <sturm@localhost.localdomain>
2017-04-18 13:36:13 +0200

    Método para poblar la base de datos con las empresas del IBEX 35

--- core/app.py
+++ core/app.py
@@ -3,7 +3,11 @@ from threading import Thread
 
 from flask import Flask
 from sqlalchemy import create_engine
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.session import sessionmaker
+
+from models.base import Base, Company
 
 
 class CoreApp(Thread):
@@ -18,22 +22,42 @@ class CoreApp(Thread):
 
     @property
     def base(self) -> dict:
-        return self._base
+        return Base
 
     def run(self):
         self.app.run(host='0.0.0.0', debug=True, port=int(os.environ.get("PORT", 5000)), threaded=True)
 
+    def seed_IBEX35(self):
+        Session = sessionmaker(bind=self.engine)
+        session = Session()
+        crs = open("/home/sturm/Workspace/Market_analizer/seeds/IBEX35.txt", "r")
+        for company_name in crs:
+            company = Company(name=company_name)
+            session.add(company)
+        try:
+            session.commit()
+        except IntegrityError as ex:
+            print(ex)
+
+    def show_data(self):
+        connection = self.engine.connect()
+        result = connection.execute("select name from Company")
+        for row in result:
+            print("Nombre:", row['name'])
+        connection.close()
+
     def __init__(self):
         self._app = Flask(__name__)
         self._app.config.from_object(__name__)
-
         self._app.config.update(dict(
-            DATABASE=os.path.join(self._app.root_path, 'database/market.db'),
+            DATABASE=os.path.join('/home/sturm/Workspace/Market_analizer', 'database/market.db'),
             SECRET_KEY='123',
             USERNAME='admin',
             PASSWORD='123'
         ))
         self._app.config.from_envvar('MARKET_SETTINGS', silent=True)
-
         self._engine = create_engine('sqlite:///' + self._app.config['DATABASE'])
-        self._base = declarative_base()
+        Base.metadata.create_all(self.engine)
+        # Base.metadata.drop_all(self.engine)
+        # self.seed_IBEX35()
+        self.show_data()
