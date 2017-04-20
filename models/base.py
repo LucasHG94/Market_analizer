81a9e0417a2b4b03c497258fc3bc17e0ae4f57fd
sturm <sturm@localhost.localdomain>
2017-04-20 13:58:14 +0200

    - Modelos actualizados. - Se ha añadido la tabla de los bonos del estado

--- models/base.py
+++ models/base.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Date
 from sqlalchemy import Float
 from sqlalchemy import ForeignKey
+from sqlalchemy.sql.schema import UniqueConstraint
 
 Base = declarative_base()
 
@@ -24,6 +25,20 @@ class BaseAPI:
         return {'success': False, 'errCode': err_code, 'data': data}
 
 
+class StateBonus(BaseAPI, Base):
+    __tablename__ = 'state_bonus'
+
+    date = Column('date', Date, nullable=False, primary_key=True)
+    type = Column('type', Float)
+    variation = Column('variation', Float)
+
+    def serialize(self) -> dict:
+        return {
+            'date': self.date,
+            'state_bonus': self.state_bonus
+        }
+
+
 class Company(BaseAPI, Base):
     __tablename__ = 'company'
 
@@ -31,27 +46,53 @@ class Company(BaseAPI, Base):
     name = Column('name', String(255), nullable=False, unique=True)
     daily_data = relationship('DailyData', lazy='subquery')
 
-    def serialize(self) -> dict:
+    def serialize(self, daily_data: bool = False) -> dict:
         return {
             'id': self.id,
             'name': self.name,
-            'dailyDatas': [daily_data.serialize() for daily_data in self.daily_data]
+            'dailyData': [daily_data.serialize() for daily_data in self.daily_data] if daily_data else None
         }
 
 
 class DailyData(BaseAPI, Base):
     __tablename__ = 'daily_data'
+    __table_args__ = (
+        UniqueConstraint('company_id', 'date'),
+    )
 
+    # TODO: poner company_id y observed_at como calves únicas
     id = Column('id', Integer, primary_key=True, autoincrement=True)
     company_id = Column('company_id', ForeignKey('company.id', ondelete='CASCADE'), nullable=False)
     company = relationship('Company', lazy='subquery', foreign_keys=[company_id])
-    observed_at = Column('observed_at', Date, nullable=False)
+    date = Column('date', Date, nullable=False)
+
+    price = Column('price', Float)
+    difference = Column('difference', Float)
+    percentage_difference = Column('percentage_difference', Float)
+    capitalization = Column('capitalization', Float)
     BPA = Column('BPA', Float)
     PER = Column('PER', Float)
     PVC = Column('PVC', Float)
     PCF = Column('PCF', Float)
     dividend_yield = Column('dividend_yield', Float)
-    state_bonus = Column('state_bonus', Float)
     interest_per_share = Column('interest_per_share', Float)
     last_value = Column('last_value', Float)
+
+    def serialize(self) -> dict:
+        return {
+            'date': self.date,
+            'companyId': self.company_id,
+            'company': self.company.serialize(),
+            'price': self.price,
+            'difference': self.difference,
+            'percentageDifference': self.percentage_difference,
+            'capitalization': self.capitalization,
+            'BPA': self.BPA,
+            'PER': self.PER,
+            'PVC': self.PVC,
+            'PCF': self.PCF,
+            'dividendYield': self.dividend_yield,
+            'interestPerShare': self.interest_per_share,
+            'lastValue': self.last_value
+        }
 
