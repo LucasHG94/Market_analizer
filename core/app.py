8acc2ccb203290c9cb631f91363180c9ed0eff4f
sturm <sturm@localhost.localdomain>
2017-04-20 13:57:40 +0200

    Método para resetear, limpiar, poblar con las compañías y con los bonos del estado la base de datos

--- core/app.py
+++ core/app.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.session import sessionmaker
 
 from models.base import Base, Company
-
+from seeds.seed import seed_IBEX35, seed_daily_data, seed_state_bonus
 
 class CoreApp(Thread):
 
@@ -27,17 +27,20 @@ class CoreApp(Thread):
     def run(self):
         self.app.run(host='0.0.0.0', debug=True, port=int(os.environ.get("PORT", 5000)), threaded=True)
 
-    def seed_IBEX35(self):
+    def seed_companies(self):
         Session = sessionmaker(bind=self.engine)
         session = Session()
-        crs = open("/home/sturm/Workspace/Market_analizer/seeds/IBEX35.txt", "r")
-        for company_name in crs:
-            company = Company(name=company_name)
-            session.add(company)
-        try:
-            session.commit()
-        except IntegrityError as ex:
-            print(ex)
+        seed_IBEX35(session)
+
+    def seed_state_bonus(self):
+        Session = sessionmaker(bind=self.engine)
+        session = Session()
+        seed_state_bonus(session)
+
+    def seed_day(self):
+        Session = sessionmaker(bind=self.engine)
+        session = Session()
+        seed_daily_data(session)
 
     def show_data(self):
         connection = self.engine.connect()
@@ -45,6 +48,21 @@ class CoreApp(Thread):
         for row in result:
             print("Nombre:", row['name'])
         connection.close()
+
+    def start_database(self):
+        Base.metadata.create_all(self.engine)
+        print('Database created')
+
+    def clean_database(self):
+        Base.metadata.drop_all(self.engine)
+        print('Database cleaned')
+
+    def restart_database(self):
+        Base.metadata.drop_all(self.engine)
+        Base.metadata.create_all(self.engine)
+        self.seed_state_bonus()
+        self.seed_companies()
+        print('Database restarted')
 
     def __init__(self):
         self._app = Flask(__name__)
@@ -57,7 +75,5 @@ class CoreApp(Thread):
         ))
         self._app.config.from_envvar('MARKET_SETTINGS', silent=True)
         self._engine = create_engine('sqlite:///' + self._app.config['DATABASE'])
-        Base.metadata.create_all(self.engine)
-        # Base.metadata.drop_all(self.engine)
-        # self.seed_IBEX35()
-        self.show_data()
+        # self.restart_database()
+        self.seed_day()
